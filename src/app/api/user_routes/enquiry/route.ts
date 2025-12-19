ef3a4716445417d314702dfb49f80782d2f1e12e
codes-swalih <codesswalih@gmail.com>
2025-12-19 15:06:08 +0530

    fix enquiry api

--- src/app/api/user_routes/enquiry/route.ts
+++ src/app/api/user_routes/enquiry/route.ts
@@ -84,4 +84,35 @@ export const POST = async (req: Request) => {
   }
 };
 
-
+export const GET = async (request: Request) => {
+  try {
+    await connectToMongoDB();
+    const allEnquiries = await enquiry.find().sort({ createdAt: -1 });
+    if (allEnquiries.length === 0) {
+      return NextResponse.json(
+        {
+          status: "Failed",
+          message: "No enquiries found",
+        },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      {
+        status: "Success",
+        message: "All enquiries fetched successfully",
+        data: allEnquiries,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error while fetching all enquiries", error);
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+        error: (error as Error).message,
+      },
+      { status: 500 }
+    );
+  }
+};
